1da9f97d0241e6bb35d1937b51b9ad2bc5cf1840
Luis Thiago Padilha <15063026@uepg.br>
2017-02-20 16:39:36 -0300

    ok

--- resources/views/admin/relatorio.blade.php
+++ resources/views/admin/relatorio.blade.php
@@ -34,21 +34,6 @@
     <a href="{{url('admin/eventos/relatorio/export/csv/'.$evento->id)}}"><button type="button" class="btn btn-success">Exportar csv</button></a>
 
     <div class="row">
-        @if (session('erro'))
-           <div class="alert alert-danger fade in alert-dismissable">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
-                 {!! session('erro') !!}
-            </div>
-               
-            
-        @endif
-        @if (session('success'))
-            <div class="alert alert-success fade in alert-dismissable">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
-                 {!! session('success') !!}
-            </div>
-               
-        @endif
         
     </div>
     <h2>Relatório</h2>
